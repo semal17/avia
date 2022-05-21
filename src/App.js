124471ca7081e4593442909b765d97320bc17187
Semen <senia-2010@mail.ru>
2022-05-21 16:42:20 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import { DATA } from './flights.js';
 import './App.css';
 
 function App() {
+  console.log(DATA[0].result.flights);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    Avia
+    
     </div>
   );
 }
