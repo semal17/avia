7f3176034c4895fa9235a23d2fb62599ff9157ef
Semen <senia-2010@mail.ru>
2022-05-28 21:39:57 +0300

    add radio buttons

--- src/components/Sort/Sort.js
+++ src/components/Sort/Sort.js
@@ -1,7 +1,45 @@
 import "./Sort.css";
 
 function Sort() {
-  return <div className="Sort">Sort</div>;
+  return (
+    <div className="sort">
+      <form className="sort-form">
+        <p className="sort-form__name">Сортировать</p>
+        <div className="sort-form__wrapper">
+          <label className="sort-form__label" for="priceUp">
+            <input
+              className="sort-form__input"
+              type="radio"
+              id="priceUp"
+              name="sort"
+              value="up"
+            />
+            - по возрастанию цены
+          </label>
+          <label className="sort-form__label" for="priceDown">
+            <input
+              className="sort-form__input"
+              type="radio"
+              id="priceDown"
+              name="sort"
+              value="down"
+            />
+            - по убыванию цены
+          </label>
+          <label className="sort-form__label" for="time">
+            <input
+              className="sort-form__input"
+              type="radio"
+              id="time"
+              name="sort"
+              value="time"
+            />
+            - по времени в пути
+          </label>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Sort;
