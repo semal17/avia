8f12cf3ce185e9519d2d5fb40e0fa3a1c6e0ec6c
Semen <senia-2010@mail.ru>
2022-06-04 13:03:53 +0300

    upd filter

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import StatusFilter from "./components/StatusFilter/StatusFilter";
 import Tickets from "./components/Tickets/Tickets";
 
 function App() {
-  const [sort, setSort] = useState("");
+  const [sort, setSort] = useState("priceUp");
   const [filter, setFilter] = useState(0);
   const [company, setCompany] = useState(0);
   const [priceFrom, setPriceFrom] = useState(0);
@@ -14,89 +14,80 @@ function App() {
   const [tickets, setTickets] = useState(DATA[0].result.flights);
 
   useEffect(() => {
-    let temp = [...tickets];
+    let temp = [...DATA[0].result.flights];
+    let arr = [];
     if (sort === "priceUp") {
-      setTickets(
-        temp.sort(
-          (a, b) =>
-            Number(a.flight.price.total.amount) -
-            Number(b.flight.price.total.amount)
-        )
+      arr = temp.sort(
+        (a, b) =>
+          Number(a.flight.price.total.amount) -
+          Number(b.flight.price.total.amount)
       );
     } else if (sort === "priceDown") {
-      setTickets(
-        temp.sort(
+      arr =         temp.sort(
           (a, b) =>
             Number(b.flight.price.total.amount) -
             Number(a.flight.price.total.amount)
-        )
+        
       );
     } else if (sort === "time") {
-      setTickets(
-        temp.sort(
+      arr =         temp.sort(
           (a, b) =>
             Number(a.flight.legs[0].duration + a.flight.legs[1].duration) -
             Number(b.flight.legs[0].duration + b.flight.legs[1].duration)
-        )
+        
       );
     } else {
-      setTickets(temp);
+      arr = temp;
     }
+    
 
-    // setTickets(
-    //   temp.filter(
-    //     (item) =>
-    //       Number(item.flight.price.total.amount) >= priceFrom &&
-    //       Number(item.flight.price.total.amount) <= priceTo
-    //   )
-    // );
-
-    if (company === 1) {
-      setTickets(temp.filter((item) => item.flight.carrier.uid === "LO"));
-    } else if (company === 2) {
-      setTickets(temp.filter((item) => item.flight.carrier.uid === "SU1"));
-    } else if (company === 3) {
-      setTickets(
-        temp.filter(
-          (item) =>
-            item.flight.carrier.uid === "SU1" ||
-            item.flight.carrier.uid === "LO"
-        )
-      );
-    } else {
-      setTickets(temp);
-    }
-  }, [sort, company]);
-
-  useEffect(() => {
-    let temp = [...tickets];    
-
-    setTickets(
-      temp.filter(
+    arr =  arr.filter(
         (item) =>
           Number(item.flight.price.total.amount) >= priceFrom &&
           Number(item.flight.price.total.amount) <= priceTo
-      )
+      
     );
 
-    
-  }, [priceFrom, priceTo]);
+
+    if (company === 1) {
+      arr = temp.filter((item) => item.flight.carrier.uid === "LO");
+    } else if (company === 2) {
+      arr = temp.filter((item) => item.flight.carrier.uid === "SU1");
+    } else if (company === 3) {
+      arr = 
+        temp.filter(
+          (item) =>
+            item.flight.carrier.uid === "SU1" ||
+            item.flight.carrier.uid === "LO"      );
+    } 
 
 
-  
-  
-  //   if(filter === 1) {
-  //     setTickets(temp.filter(item => item.flight.legs[0].segments.length + item.flight.legs[1].segments.length === 4));
-  //     // console.log(tickets[0].flight.legs[0].segments.length + tickets[0].flight.legs[1].segments.length);
+    if (filter === 1) {
+      setTickets(
+        temp.filter(
+          (item) =>
+            item.flight.legs[0].segments.length +
+              item.flight.legs[1].segments.length ===
+            4
+        )
+      );
+      console.log(tickets[0].flight.legs[0].segments.length + tickets[0].flight.legs[1].segments.length);
+    } else if (filter === 2) {
+      setTickets(
+        temp.filter(
+          (item) =>
+            item.flight.legs[0].segments.length +
+              item.flight.legs[1].segments.length ===
+              3 || 2
+        )
+      );
+    } else {
+      setTickets(temp);
+    }
 
-  //   } else if (filter === 2) {
-  //     setTickets(temp.filter(item => item.flight.legs[0].segments.length + item.flight.legs[1].segments.length === 3 || 2));
+    setTickets(arr);
 
-  //   }  else {
-  //     setTickets(temp);
-  //   }
-
-  
+  }, [sort, company, priceFrom, priceTo]);
 
   return (
     <main className="app container">
